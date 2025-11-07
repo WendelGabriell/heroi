00c917011a68125619aad6c0a51eb69be9a00b5a
WendelGabriell <gabriel.wendel733@gmail.com>
2025-11-07 01:44:38 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+class heroi {
+    constructor(nomeHeroi, idadeHeroi, tipoHeroi, tipoAtaque){
+        this.nomeHeroi = nomeHeroi;
+        this.idadeHeroi = idadeHeroi;
+        this.tipoHeroi = tipoHeroi;
+        this.tipoAtaque = tipoAtaque;
+
+    }
+
+    atacar(){
+        if (this.tipoHeroi === "mago") {
+            this.tipoAtaque = "magia";
+
+        }else if(this.tipoHeroi === "guerreiro"){
+            this.tipoAtaque = "espada";
+
+        }else if(this.tipoHeroi === "monge"){
+            this.tipoAtaque = "artes marciais";
+
+        }else if(this.tipoHeroi === "ninja"){
+            this.tipoAtaque = "shuriken";
+        }else{
+            console.log("Tipo de Herói inválido!!!, escolha entre (mago, guerreiro, monge, ninja)");
+        }
+
+        console.log(`O ${this.tipoHeroi} atacou usando ${this.tipoAtaque}`)
+    }
+}
+
+let dragonBallZ = new heroi("Kuririn", 30, "guerreiro")
+dragonBallZ.atacar();
